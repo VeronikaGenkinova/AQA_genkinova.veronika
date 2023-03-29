7a91f00b28ff6eccb39366254551de8be64a0871
veronika.genkinova <veranika-2005@ukr.net>
2023-03-29 15:39:41 +0300

    hw19

---
+++ src/main/java/org/hillel/Calculator.java
@@ -0,0 +1,20 @@
+package org.hillel;
+
+public class Calculator {
+
+    public int multiplyTest(int a, int b) {
+        return a * b;
+    }
+
+    public int divideTest(int a, int b) throws ArithmeticException {
+        return a / b;
+    }
+
+    public int addTest(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractTest(int a, int b) {
+        return a - b;
+    }
+}
